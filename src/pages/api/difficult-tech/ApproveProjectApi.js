c4a08c0502d5b9f7fdecbe2d17d7916e8fd50090
tkdrms0301 <tkdrms0301@naver.com>
2022-10-12 21:11:21 +0900

    api config modify

--- src/pages/api/difficult-tech/ApproveProjectApi.js
+++ src/pages/api/difficult-tech/ApproveProjectApi.js
@@ -1,10 +1,11 @@
-import axios from 'axios';
 import api from '../api';
+import config from '../config';
+
 const approveProjectApi = {
-    contentAll: () => api.get('api/request', null),
-    contentFilter: (filter) => api.get('api/request', { params: filter }),
-    approve: (status) => api.post('api/request/status', status),
-    reject: (status) => api.post('api/request/status', status),
-    pending: (status) => api.post('api/request/status', status)
+    contentAll: () => api.get('api/request', null, config),
+    contentFilter: (filter) => api.get('api/request', { params: filter }, config),
+    approve: (status) => api.post('api/request/status', status, config),
+    reject: (status) => api.post('api/request/status', status, config),
+    pending: (status) => api.post('api/request/status', status, config)
 };
 export default approveProjectApi;
