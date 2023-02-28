ee47bb01ce30dd9a7b08ce780e7d8f9a903e2914
tkdrms0301 <tkdrms0301@naver.com>
2023-02-28 13:59:20 +0900

    Revert "update"
    This reverts commit 1e1bd98f966ebc79867037d3e59c1b433965acba.

--- src/menu-items/hompageManage.js
+++ src/menu-items/hompageManage.js
@@ -1,13 +1,35 @@
+// assets
+import {
+    AppstoreAddOutlined,
+    AntDesignOutlined,
+    BarcodeOutlined,
+    BgColorsOutlined,
+    FontSizeOutlined,
+    LoadingOutlined
+} from '@ant-design/icons';
+
+// icons
+const icons = {
+    FontSizeOutlined,
+    BgColorsOutlined,
+    BarcodeOutlined,
+    AntDesignOutlined,
+    LoadingOutlined,
+    AppstoreAddOutlined
+};
+
+// ==============================|| MENU ITEMS - UTILITIES ||============================== //
+
 const company = {
     id: 'company',
-    title: '홈페이지',
+    title: '홈페이지 관리',
     type: 'group',
     children: [
         {
             id: 'notice',
-            title: '공지사항 작성',
+            title: '공지사항 관리',
             type: 'item',
-            url: '/notice-write',
+            url: '/notice',
             icon: ''
         }
     ]
